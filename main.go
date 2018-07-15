bf75bd07a5b9c32bc761293241d273c024317fe1
Administrator <admin@example.com>
2018-07-16 02:03:38 +0800

    v12

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"awesomeProject/logic"
 )
 
-func PrintAdd(n int){
-	for i := 0; i < n;i++{
-		if i % 2  == 1 {
-			fmt.Printf("%d\n",i)
-		}
-	}
-}
 
 
 func main() {
